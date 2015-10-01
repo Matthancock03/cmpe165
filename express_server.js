8af50b12e9bd4ac691fc17bb8767eaa76764d568
John-Franklin <johnfranklin42@gmail.com>
2015-10-01 11:40:36 -0700

    Fixed my version of the view as an exercise in understanding angular. Fixed active tab problem.

--- express_server.js
+++ express_server.js
@@ -8,5 +8,4 @@ app.use(express.static(__dirname + '/controllers'));
 app.get("/", function(req,res){
   res.status(200).sendFile(__dirname + '/views/login.html');
 });
-
 app.listen(process.env.PORT || 9000);
