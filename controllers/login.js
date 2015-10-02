5c17971b963636cf0006bfa4dac42d61cadaf7ab
Matt Hancock <matthancock03@gmail.com>
2015-10-01 21:50:48 -0700

    matt branch init

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,15 @@
-(function(){
-  var app = angular.module('Authentication',[])
+(function() {
+  var app = angular.module('Authentication', [])
 
-  app.controller('Auth', function(){
+  app.controller('Auth', function() {
     this.tab = 1;
 
-    this.setTab = function(table){
+    this.setTab = function(table) {
       this.tab = table;
       console.log("Table set to " + table);
     };
 
-    this.panelNumber = function(panel){
+    this.panelNumber = function(panel) {
       return this.tab === panel;
     };
 
