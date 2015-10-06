65a29cf5ae3a5d3f25b43527bdd7b509f72ac056
Matt Hancock <matthancock03@gmail.com>
2015-10-05 23:57:37 -0700

    init

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,5 @@
 (function(){
-  var app = angular.module('Authentication',[]).config(function ($httpProvider) {
-
-        $httpProvider.defaults.headers.common['Authorization'] = 'Basic MUVZT0xPVDVQSk5VQTlCNkZaOUcyWjdGRTo4bjluSzBkeTU4VTIyUEtVWkZQcmxpVVB0eU42bm0yZzRIYVVQdjI3Si9N';
-        delete $httpProvider.defaults.headers.common["X-Requested-With"];
-    });
+  var app = angular.module('Authentication',[])
 
   var tab = 1;
   app.controller('Auth', function(){
@@ -25,30 +21,20 @@
 
     this.submitLogin = function(email, password){
       console.log("Email: " + email + " Password: " + password);
-      var url = 'https://api.stormpath.com/v1/applications/173vkD8p8nkeJb55sXM6WW/accounts?email=' + email;
-      var response = $http.jsonp(url, {
-        method: 'GET',
-        callback: 'JSON_CALLBACK',
-        headers:{
-          "Accept" : "application/json",
-          "Authorization" : "Basic MUVZT0xPVDVQSk5VQTlCNkZaOUcyWjdGRTo4bjluSzBkeTU4VTIyUEtVWkZQcmxpVVB0eU42bm0yZzRIYVVQdjI3Si9N"
-        }
-        });
-      /*
       $http({
-      method: 'GET',
-      url: 'https://api.stormpath.com/v1/applications/173vkD8p8nkeJb55sXM6WW/accounts?email=' + email,
-      headers: {
-        "Accept" : "*",
-        "Authorization" : "Basic MUVZT0xPVDVQSk5VQTlCNkZaOUcyWjdGRTo4bjluSzBkeTU4VTIyUEtVWkZQcmxpVVB0eU42bm0yZzRIYVVQdjI3Si9N",
-    }
+      method: 'POST',
+      url: '/login',
+      data: {
+        "username": email,
+        "password": password
+      }
       }).then(function successCallback(response) {
         for (property in response) {
           console.log(property + ':' + response[property]+'; ');
         }
       }, function errorCallback(response) {
         console.log("Not working.\n\n");
-      });*/
+      });
     };
     this.panelNumber = function(panel){
       return tab === panel;
@@ -67,8 +53,7 @@
 
       $http({
       method: 'POST',
-      url: 'https://api.stormpath.com/v1/applications/173vkD8p8nkeJb55sXM6WW/accounts',
-      headers: {"Accept": "application/json", "Content-Type" : "application/json"},
+      url: '/register',
       data: {
         "givenName": firstName,
         "surname": lastName,
