e2b61bf0d1f23ec3feebb86a34e1f12d4c7f3a3d
Matt Hancock <matthancock03@gmail.com>
2015-10-07 21:38:14 -0700

    init

--- index.js
+++ index.js
@@ -2,26 +2,15 @@ var express = require('express');
 var stormpath = require('express-stormpath');
 
 var app = express();
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/controllers'));
-
-var stormpathMiddleware = stormpath.init(app, {
+app.use(stormpath.init(app, {
   application: {
     href: 'https://api.stormpath.com/v1/applications/173vkD8p8nkeJb55sXM6WW'
   },
-  expandCustomData: true,
-  enableForgotPassword: true,
-  website: {
-      login: {
-        enabled: true,
-        nextUri: __dirname + '/views/jobform.html'
-      }
-    }
-});
+  website: true
+}));
 
-app.use(stormpathMiddleware);
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/controllers'));
 
 app.get("/", function(req,res){
   res.status(200).sendFile(__dirname + '/views/login.html');
@@ -39,9 +28,8 @@ app.get("/create", function(req,res){
   res.status(200).sendFile(__dirname + '/views/jobform.html');
 })
 
-// Listen for incoming requests and serve them.
+
 app.on('stormpath.ready', function() {
-  app.listen(process.env.PORT || 9000, function() {
-    console.log("Starting server...");
-  });
+  app.listen(process.env.PORT || 9000);
+  console.log("Starting server...");
 });
