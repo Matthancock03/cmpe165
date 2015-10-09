544102d5089f807a8351f092afa0c7c905faf005
John-Franklin <johnfranklin42@gmail.com>
2015-10-09 16:42:20 -0700

    Oops
    Small change not included in revert

--- database.js
+++ database.js
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
+
 var mongoose = require('mongoose');
 
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost/social';
 mongoose.connect(url);
 console.log("Connected to MongoDB");
-=======
->>>>>>> Stashed changes
-
-var a = require('angoose');
-mongoose = a.getMongoose();
-module.exports = mongoose;
