c50a08d9931af7443f426cbb4d94bdde836d3580
John-Franklin <johnfranklin42@gmail.com>
2015-10-26 14:45:43 -0700

    Job display properly displays applications.

--- models/application.js
+++ models/application.js
@@ -5,6 +5,7 @@ var db = require('../database');
 //A class to allow users to create additions to Jobs without giving them permission to modify. neater this way.
 module.exports = db.model("Application", {jobId:{type: String, required:true},
     viewableIds:{type: [String], required:true},
+    creationDate: {type: Date, required: false, default: Date.now()},
     //Use this array in any model when you need to limit people who can view things.
     // include other viewers ONLY; don't need and can't easily access own id. ownership assumes viewability anyways
     // if undefined, system will assume anyone can view.
