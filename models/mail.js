3c4799e5e3760414644f8c822df49a149989a647
John-Franklin <johnfranklin42@gmail.com>
2015-10-28 15:11:32 -0700

    Something for Manbir to work from...
    I’ve modified the model to include a post function that has a model
    that you can access with any of the information you need to email. This
    should make your life much easier.

--- models/mail.js
+++ models/mail.js
@@ -2,14 +2,25 @@
  * Created by johnfranklin on 10/16/15.
  */
 var db = require('../database');
-module.exports = db.model("Mail", {
-    ownerId:{type: String},//=recipient, not maker. need to do it this way so users can mark as read.
+schema = db.model("Mail", {
+    ownerId:{type: String, required:true},//=recipient, not maker. need to do it this way so users can mark as read.
     body:{type: String},
     title:{type: String},
-    senderId:{type: String},//need this to allow response.
+    senderId:{type: String, required:true},//need this to allow response.
     links:{type:[String]},
     read:{type: Boolean, default:false},
     creationDate: {type: Date, required: false, default: Date.now()}
     //,terms: {type: Schema.Types.ObjectId, ref: 'Term'}
     //,comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 });
+schema.post('save', function(email) {
+    console.log('%s has been saved', email._id);
+    email.body;//the body of the email.
+    email.title;//the title
+    email.ownerId;//The email to send to. NOT THE EMAIL OF THE SENDER!
+    email.senderId;//email sent from. the id of the sender technically.
+    email.links;//the links of the email. included because angularJS will probably sanitize the information which will remove links or at least the ability for links to function.
+
+    //Manbir's code here. use whatever nodeMail stuff you need here; even if you can't get it working, the document store will still give us enough to work with on our end in the meantime.
+});
+module.exports
