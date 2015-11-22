cd980bf07ccb954ea824c0bea25db3c5f9e7b2b5
BuildTools <unconfigured@null.spigotmc.org>
2015-11-21 16:34:11 -0800

    Fixed Job Request History (need update index)

--- controllers/jobhistory.js
+++ controllers/jobhistory.js
@@ -9,59 +9,14 @@ angular.module('myApp').controller('jobHistory', function($location, $http, $sco
             $scope.loggedIn = true;
         }
         $scope.user = response.data;
-        $scope.pastJobs = Application.query({ ownerId: $scope.user.ownerId},
-
-            function(elems,err) {
-                console.log(elems);
-                $scope.pastJobs = elems;
-
-                $scope.pastJobs.sort(function(a,b){return a.jobId.localeCompare(b.jobId)});
-                var jobIdArray = $scope.pastJobs.map(function(a) {return a.jobId;})
-                console.log("Job Id Array");
-                console.log(jobIdArray);
-///////////////////
-                $scope.jobList = Job.query({
-                    },
-                    function(jobs,err)
-                    {
-                        $scope.jobList = jobs;
-                        console.log("Jobs: ");
-                        console.log(jobs);
-                        $scope.jobList.sort(function(a,b){return a._id.localeCompare(b._id)})
-                        for(var i = 0, k = 0; i < $scope.jobList.length && k < $scope.pastJobs.length;){
-                            if($scope.jobList[i].ownerId != response.data.email) {
-
-                                console.log("***ID did not match***");
-                                console.log($scope.jobList[i].title)
-                                console.log($scope.jobList[i].ownerId)
-                                console.log($scope.jobList[i].time)
-                                i++;
-                            }
-                            else {
-                                /*
-
-                                 application Date
-                                 job time
-                                 */
-                                console.log($scope.jobList[i].title)
-                                console.log($scope.jobList[i].ownerId)
-                                console.log($scope.jobList[i].time)
-                                $scope.pastJobs[k].title = $scope.jobList[i].title;
-                                $scope.pastJobs[k].email = $scope.jobList[i].ownerId;
-                                $scope.pastJobs[k].jobTime = $scope.jobList[i].time;
-
-
-
-                                k++;
-                            }
-
-
-
-                        }
-                    })
-///////////////////////
-
-            });
+        console.log($scope.user.email);
+        //console.log($scope.user.ownerId);
+        $scope.pastJobs = Job.query({
+            ownerId: $scope.user.email
+            //ownerId: $scope.user.ownerId
+        },function(elems, err){
+            console.log(elems)
+        });
 
 
     });
